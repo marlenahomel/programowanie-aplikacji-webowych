e45a13b20113fa8ce26780a7562b474661137a03
marlenahomel <49608782+marlenahomel@users.noreply.github.com>
2021-05-15 22:19:05 +0200

    LAB 2 edited
    LAB 2 edited

--- lab2/index.js
+++ lab2/index.js
@@ -7,18 +7,98 @@ var rideSound;
 var snareSound;
 var tinkSound;
 var tomSound;
+var RecordingOn1 = false;
+var RecordingOn2 = false;
+var RecordingOn3 = false;
+var RecordingOn4 = false;
 var channel1 = [];
+var channel2 = [];
+var channel3 = [];
+var channel4 = [];
+var btnChannel1Play = document.querySelector('#channel1Play');
+var btnChannel2Play = document.querySelector('#channel2Play');
+var btnChannel3Play = document.querySelector('#channel3Play');
+var btnChannel4Play = document.querySelector('#channel4Play');
+var btnChannel1Record = document.querySelector('#channel1Record');
+var btnChannel2Record = document.querySelector('#channel2Record');
+var btnChannel3Record = document.querySelector('#channel3Record');
+var btnChannel4Record = document.querySelector('#channel4Record');
 appStart();
 function appStart() {
     document.addEventListener('keypress', onKeyDown);
-    var btnChannel1Play = document.querySelector('#channel1Play');
     btnChannel1Play.addEventListener('click', onChannel1Play);
+    btnChannel2Play.addEventListener('click', onChannel2Play);
+    btnChannel3Play.addEventListener('click', onChannel3Play);
+    btnChannel4Play.addEventListener('click', onChannel4Play);
+    btnChannel1Record.addEventListener('click', onChannel1Record);
+    btnChannel2Record.addEventListener('click', onChannel2Record);
+    btnChannel3Record.addEventListener('click', onChannel3Record);
+    btnChannel4Record.addEventListener('click', onChannel4Record);
     getAudioElements();
 }
 function onChannel1Play() {
     channel1.forEach(function (sound) {
         setTimeout(function () { return playSound(sound.key); }, sound.time);
     });
+    console.log("playing channel 1");
+}
+function onChannel2Play() {
+    channel2.forEach(function (sound) {
+        setTimeout(function () { return playSound(sound.key); }, sound.time);
+    });
+    console.log("playing channel 2");
+}
+function onChannel3Play() {
+    channel3.forEach(function (sound) {
+        setTimeout(function () { return playSound(sound.key); }, sound.time);
+    });
+    console.log("playing channel 3");
+}
+function onChannel4Play() {
+    channel4.forEach(function (sound) {
+        setTimeout(function () { return playSound(sound.key); }, sound.time);
+    });
+    console.log("playing channel 4");
+}
+function onChannel1Record() {
+    if (!RecordingOn1) {
+        RecordingOn1 = true;
+        btnChannel1Record.textContent = "Recording, click to stop";
+    }
+    else if (RecordingOn1) {
+        RecordingOn1 = false;
+        btnChannel1Record.textContent = "Record on channel 1";
+    }
+}
+function onChannel2Record() {
+    if (!RecordingOn2) {
+        RecordingOn2 = true;
+        btnChannel2Record.textContent = "Recording, click to stop";
+    }
+    else if (RecordingOn2) {
+        btnChannel2Record.textContent = "Record on channel 2";
+        RecordingOn2 = false;
+    }
+}
+function onChannel3Record() {
+    if (!RecordingOn3) {
+        btnChannel3Record.textContent = "Recording, click to stop";
+        RecordingOn3 = true;
+    }
+    else if (RecordingOn3) {
+        btnChannel3Record.textContent = "Record on channel 3";
+        RecordingOn3 = false;
+    }
+}
+function onChannel4Record() {
+    if (!RecordingOn4) {
+        btnChannel4Record.textContent = "Recording, click to stop";
+        RecordingOn4 = true;
+    }
+    else if (RecordingOn4) {
+        btnChannel4Record.textContent = "Record on channel 4";
+        RecordingOn4 = false;
+    }
 }
 function getAudioElements() {
     clapSound = document.querySelector('#clap');
@@ -34,10 +114,34 @@ function getAudioElements() {
 function onKeyDown(ev) {
     var key = ev.key;
     var time = ev.timeStamp;
-    channel1.push({ key: key, time: time });
-    console.log(channel1);
-    playSound(key);
-    console.log(ev);
+    if (RecordingOn1) {
+        channel1.push({ key: key, time: time });
+        playSound(key);
+    }
+    else {
+        playSound(key);
+    }
+    if (RecordingOn2) {
+        channel2.push({ key: key, time: time });
+        playSound(key);
+    }
+    else {
+        playSound(key);
+    }
+    if (RecordingOn3) {
+        channel3.push({ key: key, time: time });
+        playSound(key);
+    }
+    else {
+        playSound(key);
+    }
+    if (RecordingOn4) {
+        channel4.push({ key: key, time: time });
+        playSound(key);
+    }
+    else {
+        playSound(key);
+    }
 }
 function playSound(key) {
     switch (key) {
--- lab2/index.ts
+++ lab2/index.ts
@@ -8,22 +8,124 @@ let snareSound: HTMLAudioElement;
 let tinkSound: HTMLAudioElement;
 let tomSound: HTMLAudioElement;
 
+let RecordingOn1 = false;
+let RecordingOn2 = false;
+let RecordingOn3 = false;
+let RecordingOn4 = false;
+
 const channel1: any[] = [];
+const channel2: any[] = [];
+const channel3: any[] = [];
+const channel4: any[] = [];
+
+const btnChannel1Play = document.querySelector('#channel1Play');
+const btnChannel2Play = document.querySelector('#channel2Play');
+const btnChannel3Play = document.querySelector('#channel3Play');
+const btnChannel4Play = document.querySelector('#channel4Play');
+
+const btnChannel1Record = document.querySelector('#channel1Record');
+const btnChannel2Record = document.querySelector('#channel2Record');
+const btnChannel3Record = document.querySelector('#channel3Record');
+const btnChannel4Record = document.querySelector('#channel4Record');
+
 
 appStart();
 function appStart(): void {
     document.addEventListener('keypress', onKeyDown);
-    const btnChannel1Play = document.querySelector('#channel1Play');
+
     btnChannel1Play.addEventListener('click', onChannel1Play);
+    btnChannel2Play.addEventListener('click', onChannel2Play);
+    btnChannel3Play.addEventListener('click', onChannel3Play);
+    btnChannel4Play.addEventListener('click', onChannel4Play);
+
+    btnChannel1Record.addEventListener('click', onChannel1Record);
+    btnChannel2Record.addEventListener('click', onChannel2Record);
+    btnChannel3Record.addEventListener('click', onChannel3Record);
+    btnChannel4Record.addEventListener('click', onChannel4Record);
+
     getAudioElements();
    
 }
 function onChannel1Play(): void {
- channel1.forEach(sound => {
-     setTimeout(() => playSound(sound.key), sound.time)
- })
-
+    channel1.forEach(sound => {
+        setTimeout(() => playSound(sound.key), sound.time)
+    })
+    console.log("playing channel 1");
 }
+
+function onChannel2Play(): void {
+    channel2.forEach(sound => {
+        setTimeout(() => playSound(sound.key), sound.time)
+    })
+    console.log("playing channel 2");
+}
+
+function onChannel3Play(): void {
+    channel3.forEach(sound => {
+        setTimeout(() => playSound(sound.key), sound.time)
+    })
+    console.log("playing channel 3");
+}
+
+function onChannel4Play(): void {
+    channel4.forEach(sound => {
+        setTimeout(() => playSound(sound.key), sound.time)
+    })
+    console.log("playing channel 4");
+}
+
+function onChannel1Record(): void {
+    if(!RecordingOn1)
+    {
+        RecordingOn1= true;
+        btnChannel1Record.textContent = "Recording, click to stop"
+    }
+    else if(RecordingOn1)
+    {
+        RecordingOn1= false;
+        btnChannel1Record.textContent = "Record on channel 1"
+    }
+}
+
+function onChannel2Record(): void {
+    if(!RecordingOn2)
+    {
+        RecordingOn2= true;
+        btnChannel2Record.textContent = "Recording, click to stop"
+    }
+    else if(RecordingOn2)
+    {
+        btnChannel2Record.textContent = "Record on channel 2"
+        RecordingOn2= false;
+    }
+}
+
+function onChannel3Record(): void {
+    if(!RecordingOn3)
+    {
+        btnChannel3Record.textContent = "Recording, click to stop"
+        RecordingOn3= true;
+    }
+    else if(RecordingOn3)
+    {   
+        btnChannel3Record.textContent = "Record on channel 3"
+        RecordingOn3= false;
+    }
+}
+
+function onChannel4Record(): void {
+    if(!RecordingOn4)
+    {
+        btnChannel4Record.textContent = "Recording, click to stop"
+        RecordingOn4= true;
+    }
+    else if(RecordingOn4)
+    {
+        btnChannel4Record.textContent = "Record on channel 4"
+        RecordingOn4= false;
+    }
+}
+
 
 function getAudioElements(){
     clapSound = document.querySelector('#clap');
@@ -41,13 +143,49 @@ function getAudioElements(){
 function onKeyDown(ev: KeyboardEvent): void {
     const key = ev.key;
     const time = ev.timeStamp;
+    if(RecordingOn1)
+    {
+    channel1.push({key, time})
+    playSound(key)
+    }
+    else
+    {
+        playSound(key)
+    }
 
-    channel1.push({ key, time });
-    console.log(channel1);
-    playSound(key);
-    console.log(ev);
+    if(RecordingOn2)
+    {
+    channel2.push({key, time})
+    playSound(key)
+    }
+    else
+    {
+        playSound(key)
+    }
 
+
+    if(RecordingOn3)
+    {
+    channel3.push({key, time})
+    playSound(key)
+    }
+    else
+    {
+        playSound(key)
+    }
+
+    if(RecordingOn4)
+    {
+    channel4.push({key, time})
+    playSound(key)
+    }
+    else
+    {
+        playSound(key)
+    }
 }
+
+
 
 function playSound(key: string): void {
     switch(key){
@@ -95,6 +233,5 @@ function playSound(key: string): void {
             break;
 
     }
-    
-
 }
+
