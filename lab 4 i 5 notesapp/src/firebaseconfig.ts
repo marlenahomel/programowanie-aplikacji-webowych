0d093352cb736ede49ac0ddb131f1b07cf5198b1
marlenahomel <49608782+marlenahomel@users.noreply.github.com>
2021-05-30 17:40:30 +0200

    lab5 changes

---
+++ lab 4 i 5 notesapp/src/firebaseconfig.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyB7q2VGw-PnhPMQ6H0WzD6W-UtSNrI34R0",
+    authDomain: "notekeep-c3aea.firebaseapp.com",
+    projectId: "notekeep-c3aea",
+    storageBucket: "notekeep-c3aea.appspot.com",
+    messagingSenderId: "171340936069",
+    appId: "1:171340936069:web:273969ddf099d83edd8aec"
+  };
